5e9d9c2248759f6a52145960ee6e3ade2e5d5342
Thomas Krueger <thomas.krueger1972@gmail.com>
2020-09-25 20:56:37 -0400

    updated patient model http views urls

--- care_connect_app/urls.py
+++ care_connect_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index)
+	path('', views.index),
+	path('search_patients', views.search_patients),
+	path('patient_programs', views.patient_programs),
+	path('patient_information', views.patient_information)
 ]
--- care_connect_app/views.py
+++ care_connect_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 def index(request):
     # retrieve info for the home page
     return render(request, 'index.html')
+
+def search_patients(request):
+    return render(request, 'search_patients.html')
+
+def patient_programs(request):
+    return render(request, 'patient_programs.html')
+
+def patient_information(request):
+    return render(request, 'patient_information.html')
