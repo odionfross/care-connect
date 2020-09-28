7e5673445c16a4f8796812a816e58fe0207d4c28
buddhiranasinghe <buddhiranasinghe@yahoo.com>
2020-09-28 11:56:23 -0700

    created 2 pages

--- care_connect_app/views.py
+++ care_connect_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def index(request):
     # retrieve info for the home page
     return render(request, 'index.html')
+
+def eligibility(request):
+	return render(request, 'eligibility.html')
+
+
+
+
+
